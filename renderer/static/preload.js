39a09131efce296c207eda87d6b175df2fb600d2
皮皮卢 <lujh@olymtech.com>
2021-06-10 15:37:19 +0800

    自动更新窗m

--- renderer/static/preload.js
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer, remote } = require('electron');
-
-window.$remote = remote;
-window.$ipcRenderer = ipcRenderer;
-
-ipcRenderer.invoke('get-main-web-content-id').then(id=>{
-  window.$mainWebContentId = id;
-})
